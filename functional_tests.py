c5a409adb7da14566521c30254efc786e8e436f2
alejandro.mesias <alejandromesiasu@gmail.com>
2018-02-10 11:06:25 -0500

    first functional test (Django worked) passed

--- functional_tests.py
+++ functional_tests.py
@@ -1,6 +1,16 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
+import unittest
 
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
+class NewVisitorTest(unittest.TestCase):
 
-assert 'Django' in browser.title
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        self.browser.get('http://localhost:8000')
+        self.assertIn('Django', self.browser.title)
+
+if __name__ == '__main__':
+    unittest.main(warnings = 'ignore')
