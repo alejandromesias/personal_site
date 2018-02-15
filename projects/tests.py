853e9f223a1847454b18ccbc3e24bc3b64b22ecb
alejandro.mesias <alejandromesiasu@gmail.com>
2018-02-15 18:10:23 -0500

    testing HomeView with context variables

--- projects/tests.py
+++ projects/tests.py
@@ -3,8 +3,11 @@ from django.test import TestCase
 class HomePageTest(TestCase):
 
     def test_uses_home_template(self):
-      response = self.client.get('/')
-      self.assertTemplateUsed(response, 'projects/am_home.html')
-      self.assertTemplateUsed(response, 'projects/base.html')
-      self.assertTemplateUsed(response, 'projects/navigation.html')
-      #self.assertTemplateUsed(response, 'projects/footer.html')
+        response = self.client.get('/')
+        self.assertTemplateUsed(response, 'projects/am_home.html')
+        self.assertTemplateUsed(response, 'projects/base.html')
+        self.assertTemplateUsed(response, 'projects/navigation.html')
+
+    def test_view_has_projects(self):
+        response = self.client.get('/')
+        self.assertContains(response,'testeando')
--- projects/views.py
+++ projects/views.py
@@ -1,4 +1,17 @@
-from django.shortcuts import render
 
-def home_page(request):
-    return render(request, 'projects/am_home.html')
+from django.http import HttpResponse
+from django.views.generic import TemplateView
+
+# Create your views here.
+class ProjectsView(TemplateView):
+    template_name = 'projects/am_home.html'
+
+    def get(self, request, *args, **kwargs):
+
+        testvar = "testeando"
+
+        context = {
+                'testvar': testvar,
+                }
+
+        return self.render_to_response(context)
